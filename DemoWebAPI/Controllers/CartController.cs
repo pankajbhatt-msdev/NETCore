79a7d9fdeb8a60e2c34a8e78355dbaad1a257b55
Pankaj Bhatt <pankajthekiwi@hotmail.com>
2026-01-09 08:31:38 +0530

    Adding Cart Saving request using Cache

--- DemoWebAPI/Controllers/CartController.cs
+++ DemoWebAPI/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using DemoWebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Text.Json;
 
 namespace DemoWebAPI.Controllers
@@ -9,6 +10,26 @@ namespace DemoWebAPI.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        private readonly IMemoryCache _memoryCache;
+        public CartController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        // GET: api/<CartController>
+        [HttpGet]
+        public ActionResult<List<Product>> Get()
+        {
+            // Retrieve products from cache
+            if (_memoryCache.TryGetValue("cartProducts", out List<Product> cachedProducts))
+            {
+                return Ok(cachedProducts);
+            }
+
+            // Return empty list if no products in cache
+            return Ok(new List<Product>());
+        }
+
         // POST api/<CartController>
         [HttpPost]
         public ActionResult<List<Product>> Post([FromBody] List<Product> products)
@@ -17,6 +38,17 @@ namespace DemoWebAPI.Controllers
             {
                 return Ok((List<Product>)null);
             }
+            // Retrieve existing products from cache
+            if (_memoryCache.TryGetValue("cartProducts", out List<Product> cachedProducts))
+            {
+                // Add new products to the existing cached products
+                cachedProducts.AddRange(products);
+                _memoryCache.Set("cartProducts", cachedProducts, TimeSpan.FromHours(1));
+                return Ok(cachedProducts);
+            }
+
+            // If no cache exists, create new cache with the products
+            _memoryCache.Set("cartProducts", products, TimeSpan.FromHours(1));
 
             return Ok(products);
         }
--- DemoWebAPI/Program.cs
+++ DemoWebAPI/Program.cs
@@ -2,6 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddMemoryCache();
+
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
